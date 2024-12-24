bc5d1f740a1125299b51eaca4c818c9d56ecde39
Jungle Jey <junglejey@Jungles-MacBook-Air.local>
2024-12-24 16:33:29 +0100

    added button general style

--- website/src/App.vue
+++ website/src/App.vue
@@ -13,6 +13,19 @@ export default {
 </script>
 
 <style>
+button {
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  transform: scale(1);
+
+  transition: all 0.2s ease-out;
+}
+
+button:hover {
+  transform: scale(1.05);
+}
+
 .d-flex {
   display: flex;
 }
